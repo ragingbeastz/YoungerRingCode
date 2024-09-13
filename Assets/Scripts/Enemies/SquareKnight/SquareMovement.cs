89fb2b19b7a418aab4c6d01bf54e443bb76af799
Dimithri Home PC <dimithriweera@gmail.com>
2024-09-13 02:05:57 +0100

    Implemented basic Health functionality: Square will reduce player health bar upon touching eachother

--- Assets/Scripts/Enemies/SquareKnight/SquareMovement.cs
+++ Assets/Scripts/Enemies/SquareKnight/SquareMovement.cs
@@ -1,13 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
+using Microsoft.Unity.VisualStudio.Editor;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Experimental.XR.Interaction;
 
 public class SquareMovement : MonoBehaviour
 {
     private Rigidbody2D characterBody;
     private Vector2 velocity;
     public GameObject Player;
+    public UnityEngine.UI.Image playerHealth;
     private float lastMovement = 0f;
+    private float lastHit = 0f;
+    private bool touchingPlayer = false;
     private int direction;
     public int speed = 8;
     public int bounceAmount = 5;
@@ -31,7 +37,9 @@ public class SquareMovement : MonoBehaviour
             Vector2 SquarePosition = transform.position;
 
             float PlayerX = PlayerPosition.x;
+            float PlayerY = PlayerPosition.x;
             float SquareX = SquarePosition.x;
+            float SquareY = SquarePosition.x;
 
             //Rightward Direction
             if (PlayerX >= SquareX){
@@ -42,6 +50,17 @@ public class SquareMovement : MonoBehaviour
                 direction = -1;
             }
 
+            // Debug.Log("Player X: " + PlayerX + "| Square X: " + SquareX + "| Difference: " + (PlayerX - SquareX));
+            float difference = PlayerX - SquareX;
+            Debug.Log(difference); 
+            if (difference <= 0.5 && difference >= -0.5){
+                touchingPlayer = true;
+            }
+            else{
+                touchingPlayer = false;
+            }
+
+            Debug.Log(touchingPlayer);
         }
 
         if ((thisMovement - lastMovement) >= 3f){
@@ -55,5 +74,33 @@ public class SquareMovement : MonoBehaviour
             
             lastMovement = thisMovement;
         }
+
+        if (thisMovement - lastHit >= 1f){
+            if (touchingPlayer){
+                Debug.Log("Collision");
+                playerHealth.fillAmount -= 0.1f;
+                lastHit = thisMovement;
+            }
+        }
     }
+
+    // void OnCollisionEnter2D(Collision2D collision)
+    // {
+    //     // Check if the player collides with the ground layer
+    //     if (collision.gameObject.layer == LayerMask.NameToLayer("Player"))
+    //     {
+    //         touchingPlayer = true;
+    //     }
+    // }
+
+    // void OnCollisionExit2D(Collision2D collision)
+    // {
+    //     // Check if the player exits collision with the ground layer
+    //     if (collision.gameObject.layer == LayerMask.NameToLayer("Player"))
+    //     {
+    //         touchingPlayer = false;
+    //     }
+    // }
 }
+
+
---
+++ Assets/Scripts/Player/HealthHandler.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+using Unity.VisualScripting;
+
+public class HealthHandler : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+}
